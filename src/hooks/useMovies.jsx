adf4d3d17884d9130e410b2d2097c25d064ca9c5
dominggo1999 <arnolddominggo19@gmail.com>
2021-12-01 15:42:20 +0700

    Modify Filter

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -1,5 +1,4 @@
-/* eslint-disable no-return-assign */
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation, useParams, useHistory } from 'react-router-dom';
 import queryString from 'query-string';
 import { baseURL, apiKey } from '../config/movieAPI';
@@ -19,7 +18,6 @@ const createURL = (type, page, params, search, filter) => {
   };
 
   let url;
-  let memoGenre;
   query.page = page || 1;
 
   if(type === 'discover') {
@@ -35,7 +33,6 @@ const createURL = (type, page, params, search, filter) => {
       const genre = genres.filter((i) => i.name.toLowerCase() === genreID)[0].id;
       query.with_genres = genre;
       url = `${baseURL}discover/movie?${queryString.stringify(query)}`;
-      memoGenre = genre;
     }
   }
 
@@ -46,14 +43,14 @@ const createURL = (type, page, params, search, filter) => {
     }
   }
 
-  return { url, memoGenre };
+  return { url };
 };
 
 const defaultFilter = 'popularity.desc';
 
 const useMovies = (type) => {
+  const history = useHistory();
   const [filter, changeFilter] = useState(defaultFilter);
-  const previousFilter = useRef(null);
 
   // need location to get query and page
   const location = useLocation();
@@ -66,53 +63,28 @@ const useMovies = (type) => {
 
   const parsed = queryString.parse(search);
   const { page, query } = parsed;
-  const { url, memoGenre } = createURL(type, page, params, query, filter);
-
-  const getMovies = async () => {
-    try {
-      // call movie api here
-      const res = await fetch(url);
-      const json = await res.json();
-      if(res.ok) {
-        setMovies(json);
-        setError(false);
-      }else{
-        setError(true);
-      }
-      setLoading(false);
-    } catch (error) {
-      setError(error);
-      console.log(error);
-    }
-  };
+  const { url } = createURL(type, page, params, query, filter);
 
   useEffect(() => {
-    let isSubscribed = true;
-
-    if(isSubscribed) {
-      changeFilter(defaultFilter);
-    }
-
-    return () => isSubscribed = false;
-  }, [memoGenre]);
-
-  useEffect(() => {
-    let isSubscribed = true;
-    const a = filter === 'popularity.desc' && previousFilter.current === 'popularity.desc';
-
-    if(filter !== previousFilter.current || a) {
-      if(isSubscribed) {
-        previousFilter.current = filter;
-        setLoading(true);
-        getMovies();
+    const getMovies = async () => {
+      try {
+        // call movie api here
+        const res = await fetch(url);
+        const json = await res.json();
+        if(res.ok) {
+          setMovies(json);
+          setError(false);
+        }else{
+          setError(true);
+        }
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        console.log(error);
       }
-    }else{
-      setLoading(true);
-    }
-
-    return () => {
-      isSubscribed = false;
     };
+    setLoading(true);
+    getMovies();
   }, [location, filter]);
 
   return {
