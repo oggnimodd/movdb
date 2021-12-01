c353c758c4eee488bb22831a57e39c9279f809f7
dominggo1999 <arnolddominggo19@gmail.com>
2021-12-02 01:36:44 +0700

    Not Found Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,48 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
+import tw, { styled } from 'twin.macro';
+import { Container } from '../shared/Flexi';
+
+export const Wrapper = styled.div`
+  ${tw`
+    flex  
+    flex-col
+    justify-center
+    items-center 
+    w-full 
+    mt-10
+  `}
+
+  h1{
+    ${tw`
+      font-primary
+      font-semibold
+    `}
+
+    font-size: 100px;
+  }
+
+  p{
+    ${tw`
+      font-secondary
+      mt-2
+      font-semibold
+    `}
+  }
+`;
 
 const NotFound = () => {
   return (
-    <div>
-
-    </div>
+    <Container>
+      <Wrapper>
+        <h1>
+          404
+        </h1>
+        <p>
+          OOPS! PAGE NOT FOUND
+        </p>
+      </Wrapper>
+    </Container>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import macrosPlugin from 'vite-plugin-babel-macros';
 
+const reactSvgPlugin = require('vite-plugin-react-svg');
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), macrosPlugin()],
+  plugins: [reactRefresh(), macrosPlugin(), reactSvgPlugin()],
   define: {
     'process.platform': JSON.stringify('win32'),
     'process.env': {},
