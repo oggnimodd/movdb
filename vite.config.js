f632e4bd85c66a3394e9fbf191efd8ce20b74091
unknown <arnolddominggo19@gmail.com>
2022-04-18 09:35:24 +0700

    remove svg plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import reactSvgPlugin from 'vite-plugin-react-svg';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +9,5 @@ export default defineConfig({
         plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
       },
     }),
-    reactSvgPlugin(),
   ],
 });
